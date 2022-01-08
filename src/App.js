bb8bef9baec7ff7a63b932f06d42fcbd080d3820
Jonayah Jackson <jonayah@thinkhalcyon.com>
2022-01-07 19:51:04 -0500

    OWM API working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import DayCard from './components/Card';
+// require('dotenv').config();
+
 
 function App() {
+  // OWM API Endpoint: https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}&units=${units}
+  const [ weatherData, setWeatherData] = useState();
+
+  // Fetch weather when component mounts
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_OWM_KEY
+    const city = 'Brooklyn'
+    const units = 'imperial'
+    const cnt = 5
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=${cnt}&APPID=${apiKey}&units=${units}`)
+      .then(res => res.json())
+      .then(res => setWeatherData(res.list.map(forecast => {
+        return {
+          day: forecast.dt_txt, // Convert to moment format 'ddd' (Tue)
+          icon: forecast.weather[0].icon,
+          max: forecast.main.temp_max, 
+          min: forecast.main.temp_min,
+        }
+      })));
+  }, []);
+
+  // See results in console
+  useEffect(() => {
+    console.log(weatherData)
+  }, [weatherData])
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <DayCard className='today' />
-        <DayCard />
-        <DayCard />
-        <DayCard />
-        <DayCard />
+        {/* If weatherData is not undefine, map out the data to DayCard */}
+        {!!weatherData && weatherData.map((i, index) => (
+          <div key={index}>
+            <DayCard day={i.day} icon={i.icon} max={i.max} min={i.min} />
+          </div>
+        ))}
+        
       </header>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
-const DayCard = () => {
+const DayCard = ({ day, icon, max, min }) => {
     return (
         <div>
             <Card className="card">
                 <Card.Body>
-                    <Card.Title className='title'>Day</Card.Title> {/* Forecast Day */}
-                    <Card.Img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" /> {/* Icon */}
+                    <Card.Title className='title'>{day}</Card.Title> {/* Forecast Day */}
+                    
+                    {/* SAMPLE ICON: http://openweathermap.org/img/wn/10d@2x.png */}
+                    <Card.Img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather icon" /> {/* Icon */}
+                    
                     <div className='temp'>
-                        <Card.Text>30&#176;</Card.Text> {/* Temp Max */}
-                        <Card.Text className='temp-min'>25&#176;</Card.Text> {/* Temp Min */}
+                        <Card.Text>{Math.round(max)}&#176;</Card.Text> {/* Temp Max */}
+                        <Card.Text className='temp-min'>{Math.round(min)}&#176;</Card.Text> {/* Temp Min */}
                     </div>
                 </Card.Body>
             </Card>
